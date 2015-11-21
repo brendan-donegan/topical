8096c46125a2a6fa96e845429242be6d25006145
Brendan Donegan <brendan.j.donegan@gmail.com>
2015-11-21 23:03:06 +0000

    Add setup.py file as we start to get serious with writing the server and client

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup
+
+setup(
+    name='topical',
+    version='0.1',
+    author='Brendan Donegan',
+    author_email='brendan.j.donegan@gmail.com',
+    description=('A client and server allowing messages to published to topics '
+                 'and read by subscribed users.')
+)
