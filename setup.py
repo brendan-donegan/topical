7027f65f6832263129d8ec38e01e8f38739f20c8
Brendan Donegan <brendan.j.donegan@gmail.com>
2015-11-21 23:07:40 +0000

    Add test suite to setup.py and fix missing __init__ file in tests directory

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
     author='Brendan Donegan',
     author_email='brendan.j.donegan@gmail.com',
     description=('A client and server allowing messages to published to topics '
-                 'and read by subscribed users.')
+                 'and read by subscribed users.'),
+    test_suite='topical.tests',
 )
